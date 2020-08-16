dbdbf90ba0a0fd2b599e64a52dd26919e1407d42
Srinivasa Chivaluri <srinivasachivaluri@Srinivasas-Air.fios-router.home>
2020-08-16 01:53:07 -0400

    changed the plot to smooth

--- vis1.R
+++ vis1.R
@@ -5,5 +5,5 @@ library(tidyverse)
 
 # +scale_x_date(labels = date_format("%m/%d"), breaks = date_breaks("2 weeks"))
 
-ggplot() + geom_line(data=plot_data, aes(caseDate, cumulative, color = countryterritoryCode))+scale_y_log10()
+ggplot() + geom_smooth(data=plot_data, aes(caseDate, cumulative, color = countryterritoryCode))+scale_y_log10()
        
